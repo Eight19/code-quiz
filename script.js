c4a8877fc07de21868366cda3eba5f5cd059d3f4
Tiffany Jones <eight19realty@gmail.com>
2023-01-03 23:42:51 -0500

    added possible answers and element for buttons

--- script.js
+++ script.js
@@ -1,27 +1,66 @@
-var questions = document.querySelectorAll(".question");
+var questionEl = document.querySelector(".question");
 var cursor = 0;
 
-// var questions = [
-    // "What does HTML mean?",
-    // "What does CSS?",
-    // "What is the role of javascript?",
-    // "What is the term used for human-written code that describes what a program is supposed to do and what the programmer wants the code to do?",
-    // "A diagram used to describe algorithms and structures of programs and programming languages?",
-    // "A process that aims to locate the exact source of errors or flaws in the code?"
-    // ];
+var questions = [
+    { Text: "What does HTML mean?", correctAnswer: "2",
+        possibleAnswers: [
+        "0. ",
+        "1. ",
+        "2. ",
+        "3. ",
+        ]
+    },
+    { Text: "What does CSS?", correctAnswer: "2",
+        possibleAnswers: [
+        "0. ",
+        "1. ",
+        "2. ",
+        "3. ",
+        ]
+    },
+    { Text: "What is the role of javascript?", correctAnswer: "0",
+        possibleAnswers: [
+        "0",
+        "1",
+        "2",
+        "3",
+        ]
+    },
+    { Text: "What is the term used for human-written code that describes what a program is supposed to do and what the programmer wants the code to do?", correctAnswer: "1",
+        possibleAnswers: [
+        "0",
+        "1",
+        "2",
+        "3",
+        ]
+    },
+    { Text: "A diagram used to describe algorithms and structures of programs and programming languages?", correctAnswer: "2",
+        possibleAnswers: [
+        "0",
+        "1",
+        "2",
+        "3",
+        ]
+    },
+    { Text: "A process that aims to locate the exact source of errors or flaws in the code?", correctAnswer: "3",
+        possibleAnswers: [
+        "0",
+        "1",
+        "2",
+        "3",
+        ]
+    },
+    ];
 
     var correctAnswers = ["2", "2", "0", "1","2", "3"];
 
     var displayQuestion = function () {
-        for (var question of questions) {
-            console.log(question);
-            if (question.dataset.index != cursor) {
-                question.style.display = "none";
-            } else {
-                question.style.display = "block";
-            }
+        questionEl.querySelector("h2").textContent = questions[cursor].Text;
+        for (var buttonLabel of questions[cursor].possibleAnswers) {
+            var buttonEl = document.createElement("button")
         }
     };
+
 
     var advance = function(event) {
         var element = event.target;
@@ -38,4 +77,4 @@ var cursor = 0;
 
     document.addEventListener("click", advance);
 
-    displayQuestion();
+   
